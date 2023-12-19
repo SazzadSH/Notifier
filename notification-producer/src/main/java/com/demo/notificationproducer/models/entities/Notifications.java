e211b1bc0025ef8b2eb31f6fa728cf5c45049891
Sazzad Hossen <sazzad.hossen@gmail.com>
2023-12-19 21:24:48 +0600

    notifications entity added

---
+++ notification-producer/src/main/java/com/demo/notificationproducer/models/entities/Notifications.java
@@ -0,0 +1,41 @@
+package com.demo.notificationproducer.models.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.Hibernate;
+
+import java.util.Date;
+import java.util.Objects;
+
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "notifications")
+public class Notifications {
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	@Column(name = "id", nullable = false, updatable = false)
+	private Long id;
+
+	private String createdBy;
+	private Date createdAt;
+
+
+	@Override
+	public boolean equals(Object o) {
+		if(this == o) return true;
+		if(o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+		Notifications that = (Notifications) o;
+		return id != null && Objects.equals(id, that.id);
+	}
+
+	@Override
+	public int hashCode() {
+		return getClass().hashCode();
+	}
+}
